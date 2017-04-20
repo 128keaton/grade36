c74ba782fa2f9e0e3bc85992abf39883ff252815
Keaton Burleson <keaton.burleson@me.com>
2017-04-20 16:51:52 -0400

    Dockerized

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   get 'index/index'
   root 'index#index'
   resources :machines
 
 get 'machine', to: 'machines#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
